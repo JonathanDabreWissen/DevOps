1f54b30f0fc9a8496f0e3b97881134390cfb980b
Jonathan Dabre <dabrejonathan.manuel@wissen.com>
2025-02-12 11:58:47 +0530

    Enhance CalculatorTest with setup and teardown methods for improved test management

--- JunitDemo/CalculatorTest.java
+++ JunitDemo/CalculatorTest.java
@@ -2,13 +2,42 @@ package com;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class CalculatorTest {
 	
+	static Calculator c1 = null;
+	
+	@BeforeAll
+	static void createCalculator() {
+		c1 = new Calculator();
+		System.out.println("-----------Started--------------");
+	}
+	
+	
+	@AfterAll
+	static void removeCalculator() {
+		c1 = null;
+		System.out.println("-----------Finished--------------");
+	}
+	
+	@BeforeEach
+	void abc() {
+		System.out.println("Before every test case");
+	}
+	
+	@AfterEach
+	void xyz() {
+		System.out.println("After every test case");
+	}
+	
 	@Test
 	void testAdd() {
-		Calculator c1 = new Calculator();
+		
 		assertEquals(30, c1.add(10, 20), ()->"Sum result is not right");
 		assertEquals(50, c1.add(30, 20), ()->"Sum result is not right");
 		assertEquals(-10, c1.add(10, -20), ()->"Sum result is not right");
@@ -16,7 +45,7 @@ class CalculatorTest {
 	
 	@Test
 	void testDiv() throws ArithmeticException {
-		Calculator c1 = new Calculator();
+		
 		assertEquals(2, c1.div(10,5));
 		assertThrows(ArithmeticException.class, ()->c1.div(90,0));
 	}
